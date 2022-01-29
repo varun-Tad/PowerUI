cb6ec181ef0b6a7223f5b0ca51ac538ee3f21359
varun-Tad <varuntad141@gmail.com>
2022-01-30 00:15:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+ 
+ const getStartedBtn = document.querySelector(".get-started-btn");
+ const gitHubBtn = document.querySelector(".github-btn");
+
+
+
+
+ getStartedBtn.addEventListener('click',()=>location.href="documentation.html");
+ gitHubBtn.addEventListener('click',()=>location.href="#");
