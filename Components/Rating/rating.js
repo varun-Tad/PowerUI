c8f67ceef511b4c7e0d174149593954e6300f876
varun-Tad <varuntad141@gmail.com>
2022-03-19 16:56:05 +0530

    added few components

---
+++ Components/Rating/rating.js
@@ -0,0 +1,35 @@
+const oneStar = document.querySelector(".one-star");
+const twoStar = document.querySelector(".two-star");
+const threeStar = document.querySelector(".three-star");
+const fourStar = document.querySelector(".four-star");
+const fiveStar = document.querySelector(".five-star");
+
+oneStar.addEventListener("click", () => {
+  oneStar.classList.add("gold-color");
+});
+
+twoStar.addEventListener("click", () => {
+  oneStar.classList.add("gold-color");
+  twoStar.classList.add("gold-color");
+});
+
+threeStar.addEventListener("click", () => {
+  oneStar.classList.add("gold-color");
+  twoStar.classList.add("gold-color");
+  threeStar.classList.add("gold-color");
+});
+
+fourStar.addEventListener("click", () => {
+  oneStar.classList.add("gold-color");
+  twoStar.classList.add("gold-color");
+  threeStar.classList.add("gold-color");
+  fourStar.classList.add("gold-color");
+});
+
+fiveStar.addEventListener("click", () => {
+  oneStar.classList.add("gold-color");
+  twoStar.classList.add("gold-color");
+  threeStar.classList.add("gold-color");
+  fourStar.classList.add("gold-color");
+  fiveStar.classList.add("gold-color");
+});
